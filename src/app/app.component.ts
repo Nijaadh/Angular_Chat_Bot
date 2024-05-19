cfbc25cd19e1a119976f559aada9eb238d67b080
Nijaadh <mohomednijaadh.net@gmail.com>
2024-05-20 00:47:40 +0530

    cahtbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ChatBotService } from './chat-bot.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'chatBot';
+
+  public userInput: string = '';
+  public messages: { text: string, user: boolean }[] = [];
+  public isDataLoaded: boolean = false;
+  public chatActive: boolean = false; // To handle the chat toggle state
+
+  constructor(private chatService: ChatBotService) {}
+
+  ngOnInit() {
+    this.chatService.loadChatData().subscribe(() => {
+      this.isDataLoaded = true;
+    });
+  }
+
+  sendMessage() {
+    if (this.userInput.trim() !== '' && this.isDataLoaded) {
+      this.messages.push({ text: this.userInput, user: true });
+      const botResponse = this.chatService.getResponse(this.userInput);
+      this.messages.push({ text: botResponse, user: false });
+      this.userInput = '';
+    } else if (!this.isDataLoaded) {
+      this.messages.push({ text: "Data is still loading. Please wait.", user: false });
+    }
+  }
+
+  toggleChat() {
+    this.chatActive = !this.chatActive;
+  }
 }
--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ChatBotService } from './chat-bot.service';
 
 @NgModule({
   imports: [
@@ -11,4 +12,28 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent],
 })
-export class AppServerModule {}
+export class AppServerModule implements OnInit{
+  
+  userInput: string = '';
+  messages: { text: string, user: boolean }[] = [];
+  isDataLoaded: boolean = false;
+
+  constructor(private chatService: ChatBotService) {}
+  ngOnInit() {
+    this.chatService.loadChatData().subscribe(() => {
+      this.isDataLoaded = true;
+    });
+  }
+
+  sendMessage() {
+    console.log(this.userInput);
+    if (this.userInput.trim() !== '' && this.isDataLoaded) {
+      this.messages.push({ text: this.userInput, user: true });
+      const botResponse = this.chatService.getResponse(this.userInput);
+      this.messages.push({ text: botResponse, user: false });
+      this.userInput = '';
+    } else if (!this.isDataLoaded) {
+      this.messages.push({ text: "Data is still loading. Please wait.", user: false });
+    }
+  }
+}
