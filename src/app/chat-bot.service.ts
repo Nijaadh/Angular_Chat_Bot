32b5998ddabba87023dca79b039fde36d4d064fb
Nijaadh <mohomednijaadh.net@gmail.com>
2024-05-21 05:00:08 +0530

    updated

--- src/app/chat-bot.service.ts
+++ src/app/chat-bot.service.ts
@@ -44,18 +44,6 @@ export class ChatBotService {
   async generateResponseWithGemini(message: string): Promise<string> {
     const json=this.chatData.intents;
     const prompt ="my shop name is MLT Holdings PVT LTD it is cosmetic shop, situated in nittambuwa open till 8.00am to night 8.00pm  some people ask some questions regarding cosmetics, and reagarding shop informations some peoples ask some un wanted questions not related to the cosmetics so if it is looks related to a cosmetic question can you give me a samll, prety and wise answer for that particular question ? if it is unwanted question or not related to cosmetics always tell sorry please kind to ask related to cosmetic problems and shop inquiries only in this time customer ask"+message+"the patterns are like this  ->  "+json
-    // try {
-    //   const headers = new HttpHeaders({
-    //     'Content-Type': 'application/json',
-    //     'Authorization': 'AIzaSyD3odXbQFR_BBs2hWBASOiavxhjc3Gyq4M' // Replace YOUR_GPT_API_KEY_HERE with your actual API key
-    //   });
-    //   const requestOptions = { headers: headers };
-    //   const response = await this.http.post<any>(this.GeminiURL, { prompt: message }, requestOptions).toPromise();
-    //   return response.choices[0].text.trim(); // Assuming the response from ChatGPT API contains a 'text' field
-    // } catch (error) {
-    //   console.error("Error occurred while fetching response from ChatGPT:", error);
-    //   return "Sorry, I'm still learning. Can you please rephrase your question?";
-    // }
 
     try {
        const model = this.genarativeAI.getGenerativeModel({model:'gemini-pro'});
